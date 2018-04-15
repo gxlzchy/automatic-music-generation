aa362f51bf30fe4c32b69d66930d590a46c5d0de
gxlzchy <gxlzchy@gmail.com>
2018-04-15 20:53:57 +0800

    add 2211 and 2121 cases. Restructed demo folder according to report

--- README.md
+++ README.md
@@ -5,15 +5,4 @@
 # OBJECTIVE
 
   The objective of this project is to build a music composition system using machine learning algorithms. The scale of this system should be small in a sense that it generates a piece of short melody. The generated music can not only be used for further evaluation, but also serve as an input to a larger scale music generator to compose longer music. The generated music demonstrates explicit patterns similar to the inputted human composed music. The equipment needed for this system is a normal computer.
-   
-  This project will make attempt at a combined approach for music composition system and music evaluation system, i.e., to integrate two subsystems music generating system and music verification system into one. Since both topics are subjective to human evaluation, this project are expected to quantify the evaluation process. Once the performance can be evaluated, the project will be able to optimize the models in the sense of maximizing the quantitative performance.
-    
-# MUSIC VERIFICATION SYSTEM
-
-  The function of the music verification system is to verify the outputted music of the generating system. This system distinguishes music from random noise. This system is important since it ensures the output quality of the whole system. This part should be implemented first since it is independent for the music generating system. Input: The data collected in music generating system or a piece of melody generated in the music generating system. The length of input is a musical sentence 8 measures.
-
-Output: 0 or 1, indicating whether the inputted music is music or noise.
-
-Model: The system is implemented using multiple Hidden Markov Models.
- 
-  Hidden Markov Model is a powerful tool to model stochastic process over time. For this system, we adopt multiple HMMs and a final boosting. It is suggested that Long Short-Term Memory Network LSTM is a possible method to generate music. It may as well applied to classification of music and noise. Due to the limited number of data and the scale of this system, LSTM is not adopted in this project.
+  
--- evaluation.m
+++ evaluation.m
@@ -7,15 +7,16 @@ addpath('D:\OneDrive\Year 4\COMP4911 CAPSTONE PROJECT\src\midi');
 clc;
 close all;
 tic;
-
-input_distr=zeros(1,326);
-output_distr=zeros(1,326);
+n=1000;
+cnt=0;
 prob=[];
+avg_cossim=0;
 %--------------------------------------------
-for i=1:3
+for i=1:n
+    input_distr=zeros(1,326);
     i
     try
-        midi=readmidi(strcat('E:\capstone project large files\midi data\training_game_copy\train (',int2str(i),').mid'));
+        midi=readmidi(strcat('D:\OneDrive\Year 4\COMP4911 CAPSTONE PROJECT\src\training\training_mix\train (',int2str(i),').mid'));
         notes = midiInfo(midi,0);
         %Converted Note Number Distribution
         notes=normalization(notes);
@@ -28,36 +29,43 @@ for i=1:3
         k=k+1;
         input_distr(X(k))=input_distr(X(k))+1;
     
+    
+    
+
+    %--------------------------------------------
+    %output music
+    for j=1:20
+        output_distr=zeros(1,326);
+        midi=readmidi(strcat('D:\OneDrive\Year 4\COMP4911 CAPSTONE PROJECT\src\output\formal output\1000_mix_1_times\1000_mix_1Times (',int2str(j),').mid'));
+        notes = midiInfo(midi,0);
+        %Converted Note Number Distribution
+        notes=normalization(notes);
+        notes=chord_classification(notes);
+        X=notes(:,3)';
+        X=fix(X);
+        for k=1:size(X,2)-1
+            output_distr(X(k))=output_distr(X(k))+1;
+        end
+        k=k+1;
+        output_distr(X(k))=output_distr(X(k))+1;
+
+        %--------------------------------------------
+        %cosine similarity
+        cossim=1-pdist2(input_distr,output_distr,'cosine');
+        if ~isnan(cossim)
+            avg_cossim=avg_cossim+cossim;
+            cnt=cnt+1;
+        end
+        %--------------------------------------------
+    end
     catch ME
         warning(strcat('Problem with ',int2str(i)));
         prob=[prob i];
         disp(ME.message);
     end
-    
 end
-%--------------------------------------------
-%output music
-for i=1:20
-    midi=readmidi(strcat('D:\OneDrive\Year 4\COMP4911 CAPSTONE PROJECT\src\output\formal output\3_game_100_times\3_game_100Times (',int2str(i),').mid'));
-    notes = midiInfo(midi,0);
-    %Converted Note Number Distribution
-    notes=normalization(notes);
-    notes=chord_classification(notes);
-    X=notes(:,3)';
-    X=fix(X);
-    for k=1:size(X,2)-1
-        output_distr(X(k))=output_distr(X(k))+1;
-    end
-    k=k+1;
-    output_distr(X(k))=output_distr(X(k))+1;
-end
-%--------------------------------------------
-%cosine similarity
-cossim=1-pdist2(input_distr,output_distr,'cosine');
-cossim
-%--------------------------------------------
-
-
+avg_cossim=avg_cossim/cnt;
+avg_cossim
 toc;
 % 
 % 
